b9ed8f29308adaddf87b061cb6df25a42d471054
Timothy <shanmo@users.noreply.github.com>
2022-10-30 15:32:13 +0800

    add

---
+++ ch04/ single_list.rs
@@ -0,0 +1,49 @@
+use std::cell::RefCell; 
+use std::rc::Rc; 
+
+// Rc is a pointer with shared ownership while RefCell provides interior mutability.
+// Because RefCell<T> allows mutable borrows checked at runtime, you can mutate the value inside the RefCell<T> even when the RefCell<T> is immutable.
+type SingleLink = Option<Rc<RefCell<Node>>>; 
+
+#[derive(Clone)]
+struct Node {
+    value: String, 
+    next: SingleLink, 
+}
+
+impl Node {
+    fn new(value: String) -> Rc<RefCell<Node>> {
+        Rc::new(RefCell::new(
+            Node {
+                value: value, 
+                next: None, 
+            }
+        ))
+    }
+}
+
+struct TransactionLog {
+    head: SingleLink, 
+    tail: SingleLink, 
+    pub length: u64
+}
+
+impl TransactionLog {
+    pub fn new_empty() -> TransactionLog {
+        TransactionLog {
+            head: None, 
+            tail: None, 
+            length: 0
+        }
+    }
+
+    pub fn append(&mut self, value: String) {
+        let new = Node::new(value); 
+        match self.tail.take() {
+            Some(old) => old.borrow_mut().next = Some(new.clone()),
+            None => self.head = Some(new.clone()); 
+        }; 
+        self.length += 1; 
+        self.tail = Some(new); 
+    }
+}
